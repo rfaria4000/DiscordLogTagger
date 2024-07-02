27fb7d76fda09c9b07d45e9ae9ac3b87164ec986
Richard Faria <richardfaria4000@gmail.com>
2024-07-02 15:46:47 -0400

    Replace messsage handler with wakeup message

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ load_dotenv(".env")
 DISCORD_TOKEN = os.environ.get("ENV_DISCORD_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
+# intents.message_content = True
 
 client = discord.Client(intents=intents)
 
 @client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+async def on_ready():
+  print(f'{client.user} has connnected to Discord')
+
+# @client.event
+# async def on_message(message):
+#   if message.author == client.user:
+#     return
   
-  if message.content.startsWith('$hello'):
-    await message.channel.send('Hello!')
+#   if message.content.startsWith('$hello'):
+#     await message.channel.send('Hello!')
 
 client.run(DISCORD_TOKEN)
