3a34d6b0ab5467fa1817c47d3afeda89593a85e4
Richard Faria <richardfaria4000@gmail.com>
2024-07-02 15:46:47 -0400

    Implement bugged version of message listener

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import os
 from dotenv import load_dotenv
 
+import discord
+
 load_dotenv(".env")
 DISCORD_TOKEN = os.environ.get("ENV_DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startsWith('$hello'):
+    await message.channel.send('Hello!')
+
+client.run(DISCORD_TOKEN)
