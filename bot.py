aa6e4880959c9a5e792b4e2d316e11347c1cdd99
Richard Faria <richardfaria4000@gmail.com>
2024-08-09 17:52:35 -0400

    Add global sync command to bot

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ class DiscordBot(commands.Bot):
     await self.load_extension(f"cogs.test")
     await self.load_extension(f"cogs.sync")
     await bot.tree.sync(guild=discord.Object(os.getenv("DISCORD_GUILD_ID")))
-    await bot.tree.sync()
+    # await bot.tree.sync()
 
   async def on_ready(self):
     print(f'{self.user} has connected to Discord!')
--- cogs/sync.py
+++ cogs/sync.py
@@ -1,25 +1,29 @@
 import discord, os
-from discord import app_commands
 from discord.ext import commands
+from discord.ext.commands import Context
 from dotenv import load_dotenv
 
 load_dotenv(".env")
-
 MY_GUILD_ID = discord.Object(os.getenv("DISCORD_GUILD_ID"))
 
 class sync(commands.Cog):
   def __init__(self, bot: commands.Bot) -> None:
     self.bot = bot
   
-  @app_commands.command(
+  # NOTE: not an app command
+  @commands.command(
       name="sync",
-      description="Sync the command tree globally."
+      description="Synchronizes slash commands."
   )
-  @app_commands.guilds(MY_GUILD_ID)
   @commands.is_owner()
-  async def sync(self, interaction: discord.Interaction) -> None:
+  async def sync(self, context: Context) -> None:
     print("called sync command")
-    await interaction.response.send_message("Successfully created")
+    await context.bot.tree.sync()
+    embed = discord.Embed(
+      description="Successfully created, restart Discord to see updates.",
+      color=0x3EB489
+    )
+    await context.reply(embed=embed, mention_author=False)
 
 async def setup(bot: commands.Bot) -> None:
   await bot.add_cog(sync(bot))
