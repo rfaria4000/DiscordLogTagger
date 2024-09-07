daafa812cf2f8f453ac4e777ef07b2ccb98d82a9
Richard Faria <richardfaria4000@gmail.com>
2024-09-07 19:45:15 -0400

    Init test files for single and multi fight embeds

--- tests/test_embed_compilation.py
+++ tests/test_embed_compilation.py
@@ -1,4 +1,3 @@
-import pytest
 import os, json
 import embed
 
---
+++ tests/test_embed_single.py
@@ -0,0 +1,12 @@
+import os, json
+import embed
+
+class TestSingle:
+  def setup_method(self, method):
+    pass
+
+  def teardown_method(self, method):
+    pass
+
+  def test_single_name(self):
+    assert()
