10f01010277b70353a138a4f9aa4630d20a53fd4
Richard Faria <richardfaria4000@gmail.com>
2024-09-18 22:36:06 -0400

    Scaffold parametrization of test_embed

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,22 +1,24 @@
 import os, json
 import embed
+import pytest
 
-class TestEmbed:
-  #TODO: parametrize this for everything in the test data folder
-  def setup_method(self, method):
-    pass
-
-  def teardown_method(self, method):
-    pass
+@pytest.mark.parametrize("blank", (None, None))
+class TestEmbed:  
+  @pytest.fixture(scope="function", autouse=True)
+  def setup_and_teardown(self, blank):
+    print("setup")
+    yield
+    print("teardown")
 
   #TODO: Extract methods from test_embed_compilation
-  def test_embed_author(self):
-    assert()
+  def test_embed_author(self, blank):
+    print("inside test")
+    # assert()
   
-  def test_embed_link(self):
-    assert()
+  # def test_embed_link(self):
+  #   assert()
 
-  def test_embed_time(self):
-    assert()
+  # def test_embed_time(self):
+  #   assert()
 
   
