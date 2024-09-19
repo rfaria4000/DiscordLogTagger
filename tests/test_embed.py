feb75f1cc0bc83e73203065aa58bb5048a1a6c55
Richard Faria <richardfaria4000@gmail.com>
2024-09-18 23:56:43 -0400

    Parametrize tests using test_data for test_embed

--- tests/test_embed.py
+++ tests/test_embed.py
@@ -1,20 +1,42 @@
-import os, json
-import embed
+import os
+import json
 import pytest
+import embed
 
-@pytest.mark.parametrize("blank", (None, None))
+def get_test_data():
+  test_data = []
+  dir = os.path.dirname(__file__)
+  # for file in os.listdir():
+  with os.scandir(os.path.join(dir, 'test_data/')) as test_folder_iterator:
+    for test_entry in test_folder_iterator:
+      with open(test_entry.path) as test:
+        test_data.append(json.load(test))
+  
+  return test_data
+
+print(get_test_data())
+
+@pytest.mark.parametrize("test_data", get_test_data())
 class TestEmbed:  
   @pytest.fixture(scope="function", autouse=True)
-  def setup_and_teardown(self, blank):
+  def setup_and_teardown(self, test_data):
     print("setup")
+    self.report = test_data
+    self.code = self.report["data"]["reportData"]["report"]["code"]
+    self.link = f"https://www.fflogs.com/reports/{self.code}"
+    self.embed = embed.generateEmbed(self.report, self.link)
+    
     yield
+    
     print("teardown")
+    del self.report, self.code, self.link, self.embed
 
   #TODO: Extract methods from test_embed_compilation
-  def test_embed_author(self, blank):
+  def test_embed_author(self, test_data):
     print("inside test")
-    # assert()
-  
+    reportOwner = self.report["data"]["reportData"]["report"]["owner"]["name"]
+    assert self.embed.author.name == f"Uploaded by {reportOwner}"
+
   # def test_embed_link(self):
   #   assert()
 
